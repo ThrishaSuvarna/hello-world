b900956cf42ccaf0a581c0bfe82879836b75c3a7
Thrisha <166195432+ThrishaSuvarna@users.noreply.github.com>
2026-02-04 11:01:21 +0530

    Add inventory management functionality

---
+++ src/Day-3 Task 1.py
@@ -0,0 +1,17 @@
+
+inventory = ["Apples", "Bananas", "Carrots", "Dates"]
+
+
+print("Current Inventory:", inventory)
+
+
+inventory.append("Eggs")
+
+
+inventory.remove("Bananas")
+
+
+inventory.sort()
+
+
+print("Final Updated Inventory:", inventory)
