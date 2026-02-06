0ebdf37dffecee1fbdf67f6a099f7f36ead6625a
Thrisha <166195432+ThrishaSuvarna@users.noreply.github.com>
2026-02-06 14:13:07 +0530

    Add rectangle area and perimeter calculation

---
+++ src/Day-5 Task 1.py
@@ -0,0 +1,13 @@
+def calc_rectangle(length, width):
+    area = length * width
+    perimeter = 2 * (length + width)
+    return area, perimeter   
+
+length = float(input("Enter length: "))
+width = float(input("Enter width: "))
+
+
+area, perimeter = calc_rectangle(length, width)
+
+
+print("Area:", area, ", Perimeter:", perimeter)
