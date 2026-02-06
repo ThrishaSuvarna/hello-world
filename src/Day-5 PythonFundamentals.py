c0f19034bebbdff6be5bc5537c20f10b8f06448d
Thrisha <166195432+ThrishaSuvarna@users.noreply.github.com>
2026-02-06 14:15:15 +0530

    Add Python fundamentals script with functions and imports

---
+++ src/Day-5 PythonFundamentals.py
@@ -0,0 +1,37 @@
+"""
+def greet():
+    print("Hello,Welcoome to the Internship!")
+
+greet()
+
+def add_numbers(a,b):
+    return a+b
+result=add_numbers(5,3)
+print(result)
+
+x=10
+def show_value():
+    print(x)
+show_value()
+print(x)
+
+food="Pizza"
+def junkfood():
+    food="burger"
+    juice="pista"
+    print(food,"is not good for helath")
+junkfood()
+
+print(food,"is not good for health")
+"""
+
+
+import math
+import random
+print(math.sqrt(16))
+print(random.randint(1,10))
+
+
+
+
+
